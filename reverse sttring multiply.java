2b0161f376857515c34c7d3de0984e609c6d7289
Shubham Srivastava <35880101+adisri06@users.noreply.github.com>
2024-08-25 00:14:58 +0530

    Create reverse sttring multiply.java

---
+++ reverse sttring multiply.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] leftProducts = new int[n];
+        int[] rightProducts = new int[n];
+        int[] answer = new int[n];
+        leftProducts[0] = 1;
+        for (int i = 1; i < n; i++) {
+            leftProducts[i] = leftProducts[i - 1] * nums[i - 1];
+             System.out.print("\nleft products of " + i +"  " + leftProducts[i]); 
+
+        }
+        rightProducts[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            rightProducts[i] = rightProducts[i + 1] * nums[i + 1];
+            System.out.print("\nrightProducts of " + i +"  " + rightProducts[i]); 
+}
+        for (int i = 0; i < n; i++) {
+            answer[i] = leftProducts[i] * rightProducts[i];
+            System.out.print("\nanswer of " + i +"  " + answer[i]); 
+
+        
+        
+
+        }
+                return answer;
+        
+    }
+}
+
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+// Example 2:
+
+// Input: nums = [-1,1,0,-3,3]
+// Output: [0,0,9,0,0]
